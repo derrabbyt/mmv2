6c0badc50473136a52484c778160089cf3258604
derrabbyt <florian.rammerstorfer1@gmail.com>
2026-02-25 20:17:33 +0100

    added endpoints and repository folders with meetup.py and poi.py files with placeholder code, routes can now be registered in www.py

--- backend/backend/www.py
+++ backend/backend/www.py
@@ -1,19 +1,19 @@
 import os
-
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 
+from backend.endpoints import meetup, poi
+
 app = FastAPI(root_path="/api")
 
-static_file_path = os.path.dirname(os.path.realpath(__file__)) + "/static"
+static_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "static")
 app.mount("/static", StaticFiles(directory=static_file_path), name="static")
 
+# endpoint registration 
+app.include_router(meetup.router, prefix="/meetup", tags=["meetup"])
+app.include_router(poi.router, prefix="/poi", tags=["poi"])
 
 @app.get("/", include_in_schema=False)
 async def root() -> RedirectResponse:
     return RedirectResponse("/docs")
-
-@app.get("/hello")
-async def hello_world():
-    return {"message": "Hello, World!"}
